e1aca6b6ba38293c32f95e97bdd81c747062fe46
Taha Meskini <taha.meskini@ominimo.eu>
2026-07-13 10:40:31 +0200

    chore:upgrade to tailwind V4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), tailwindcss(), viteTsconfigPaths()],
   server: {
     port: 3000,
   },
